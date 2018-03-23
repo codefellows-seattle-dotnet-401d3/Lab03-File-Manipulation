8c8edbb079e3c3acadff32f0b7601e621fa521c2
Playingtygre <kymiehsu123@gmail.com>
2018-03-22 23:55:25 -0700

    testing file path

--- ConsoleApp1/WordGuess/Program.cs
+++ ConsoleApp1/WordGuess/Program.cs
@@ -7,6 +7,45 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            ReadFile();
+
         }
+
+
+        public static void CreateAFile(string path)
+        {
+
+            byte[] words = new UTF8Encoding(true).GetBytes("Test");
+
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    sw.Write("This is a test");
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            using (FileStream fs = File.Create(path))
+            {
+                fs.Write(words, 0, words.Length);
+            }
+
+        }
+
+
+
+
+
+
+
+
+
     }
+    
 }
