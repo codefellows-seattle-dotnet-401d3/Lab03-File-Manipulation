d260155d431c67fb78fe1de716042bbb482e72a5
Playingtygre <kymiehsu123@gmail.com>
2018-03-31 08:32:24 -0700

    setting up void tests

--- Word_Guessing/XUnitTestProject1/UnitTest1.cs
+++ Word_Guessing/XUnitTestProject1/UnitTest1.cs
@@ -1,14 +1,51 @@
 using System;
 using Xunit;
+using Word_Guessing;
+using static Word_Guessing.Program;
 
 namespace XUnitTestProject1
 {
     public class UnitTest1
     {
+        /*          NON-WORKING THEORY
+        [Theory]
+        [InlineData("blue")]
+
+        public void CanReturnNum(string words)
+        {
+            Random rnd = new Random();
+            Assert.Equal("blue", SetInitial(wordsd));
+        }*/
+
+        /*          NON-WORKING THEORY
+        [Theory]
+        //method to check pass new array of words into SetInitial method
+        [InlineData(myarray)]
+
+        public void Check(Array myArray[])
+        {
+            string[] newArray = new string[];
+            Assert.Equal("blue", Program.SetInitial(newArray));
+        }*/
+
+
+
         [Fact]
         public void Test1()
         {
-
+            string [] newarray = new string[] { "josh" };
+            Assert.Equal("josh", Program.SetInitial(newarray));
         }
+
+
+
+        [Fact]
+        public void Test2()
+        {
+            string[] anotherArray = new string[] { "this" };
+            Assert.Equal("this", Program.SetInitial(anotherArray));
+        }
+
+
     }
 }
